5714bc5658b8250b3d9887f39e83a130b7a2ae29
mezii <deptrai.team.pro@gmail.com>
2020-09-11 17:13:45 +0700

    S

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '254c446e4160ac387f13c7ee67730eef'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9cd2956a-a859-4dbb-885b-8750269897c7'
+};
