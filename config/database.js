118b9bb5f79bad19d7bb812ab9ba455a9e475db3
mezii <deptrai.team.pro@gmail.com>
2020-09-11 18:46:04 +0700

    Fix Database

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
+        database: env('DATABASE_NAME', 'mezii'),
         username: env('DATABASE_USERNAME', 'mezii'),
-        password: env('DATABASE_PASSWORD', 'Minhhuydeptrai1'),
+        password: env('DATABASE_PASSWORD', 'huyhuy'),
       },
       options: {
         ssl: false,
